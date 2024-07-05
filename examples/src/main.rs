0aa8c93fcc75c83afa2de02d494d373abeb7d50d
filipton <filipton12@gmail.com>
2024-07-05 17:11:08 +0200

    explore: connection checks before wifi tasks spawn

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -4,15 +4,19 @@
 
 use embassy_executor::Spawner;
 use embassy_net::{tcp::TcpSocket, Config, Stack, StackResources};
-use embassy_time::{Duration, Timer};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
+use embassy_time::{with_timeout, Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, peripherals::Peripherals, prelude::*, system::SystemControl,
-    timer::timg::TimerGroup,
+    clock::ClockControl, peripheral::Peripheral, peripherals::Peripherals, prelude::*,
+    system::SystemControl, timer::timg::TimerGroup,
 };
-use esp_wifi::wifi::{
-    ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-    WifiState,
+use esp_wifi::{
+    ble::controller,
+    wifi::{
+        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
+        WifiState,
+    },
 };
 use static_cell::make_static;
 
@@ -23,6 +27,8 @@ const RX_BUFFER_SIZE: usize = 16384;
 const TX_BUFFER_SIZE: usize = 16384;
 static mut TX_BUFF: [u8; TX_BUFFER_SIZE] = [0; TX_BUFFER_SIZE];
 static mut RX_BUFF: [u8; RX_BUFFER_SIZE] = [0; RX_BUFFER_SIZE];
+
+static WIFI_SIG: Signal<CriticalSectionRawMutex, u32> = Signal::new();
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -49,8 +55,12 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
     esp_hal_embassy::init(&clocks, timg0);
 
-    let wifi = peripherals.WIFI;
+    let mut wifi = peripherals.WIFI;
+    let wifi_cloned = unsafe { wifi.clone_unchecked() };
     let (_wifi_ap, wifi_interface, controller) = esp_wifi::wifi::new_ap_sta(&init, wifi).unwrap();
+
+    let (wifi_interface, mut controller) =
+        esp_wifi::wifi::new_with_mode(&init, wifi_cloned, WifiStaDevice).unwrap();
 
     let config = Config::dhcpv4(Default::default());
     let seed = 69420;
@@ -61,6 +71,48 @@ async fn main(spawner: Spawner) {
         make_static!(StackResources::<3>::new()),
         seed,
     ));
+
+    /*
+    while !matches!(controller.is_started(), Ok(true)) {
+        log::info!("dsa");
+        Timer::after_millis(5).await;
+    }
+    */
+
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: WIFI_SSID.try_into().expect("Wifi ssid parse"),
+        password: WIFI_PSK.try_into().expect("Wifi psk parse"),
+        ..Default::default()
+    });
+    controller.set_configuration(&client_config).unwrap();
+    log::info!("Starting wifi");
+    controller.start().await.unwrap();
+    log::info!("Wifi started!");
+
+    log::info!("About to connect...");
+    let start_time = embassy_time::Instant::now();
+    loop {
+        if start_time.elapsed().as_secs() > 15 {
+            log::warn!("Connect timeout!");
+            break;
+        }
+
+        match with_timeout(Duration::from_secs(15), controller.connect()).await {
+            Ok(res) => match res {
+                Ok(_) => {
+                    log::info!("Wifi connected!");
+                    break;
+                }
+                Err(e) => {
+                    log::info!("Failed to connect to wifi: {e:?}");
+                }
+            },
+            Err(_) => {
+                log::warn!("Connect timeout.1");
+                break;
+            }
+        }
+    }
 
     spawner
         .spawn(connection(controller, stack))
@@ -76,6 +128,8 @@ async fn main(spawner: Spawner) {
             break;
         }
     }
+
+    Timer::after_millis(15000).await;
 
     /*
     let mut socket = unsafe {
@@ -104,11 +158,21 @@ async fn connection(
     log::info!("start connection task");
     log::info!("Device capabilities: {:?}", controller.get_capabilities());
     loop {
-        if esp_wifi::wifi::get_wifi_state() == WifiState::StaConnected {
-            // wait until we're no longer connected
-            controller.wait_for_event(WifiEvent::StaDisconnected).await;
-            Timer::after(Duration::from_millis(5000)).await
+        /*
+        if WIFI_SIG.signaled() {
+            log::warn!("Signaled: {:?}", WIFI_SIG.wait().await);
+            break;
         }
+        */
+
+        if esp_wifi::wifi::get_wifi_state() == WifiState::StaConnected {
+            // log::info!("w8");
+            // wait until we're no longer connected
+            //controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            Timer::after(Duration::from_millis(100)).await;
+            continue;
+        }
+
         if !matches!(controller.is_started(), Ok(true)) {
             let client_config = Configuration::Client(ClientConfiguration {
                 ssid: WIFI_SSID.try_into().expect("Wifi ssid parse"),
