896b1754f803e5a9297c9c24368307fc8ada4e82
filipton <filipton12@gmail.com>
2025-07-16 21:04:08 +0200

    wip: disable ble after wifi connected

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -81,7 +81,14 @@ pub async fn bluetooth_task(
                     let a = gatt_events_task(&server, &conn, &signals);
                     let b = custom_task(&server, &conn, &stack, &signals);
 
-                    embassy_futures::select::select(a, b).await;
+                    let res = embassy_futures::select::select(a, b).await;
+                    match res {
+                        First(_) => {}
+                        Second(_) => {
+                            // wifi connected returns custom_task
+                            break;
+                        }
+                    }
                 }
                 Err(e) => {
                     log::error!("[adv] error: {:?}", e);
@@ -90,111 +97,6 @@ pub async fn bluetooth_task(
         }
     })
     .await;
-
-    /*
-        let now = || {
-            esp_hal::time::Instant::now()
-                .duration_since_epoch()
-                .as_millis()
-        };
-
-        let mut ble = Ble::new(connector, now);
-        loop {
-            _ = ble.init().await;
-            _ = ble.cmd_set_le_advertising_parameters().await;
-            _ = ble
-                .cmd_set_le_advertising_data(
-                    create_advertising_data(&[
-                        AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
-                        AdStructure::ServiceUuids16(&[Uuid::Uuid16(0xf254)]),
-                        AdStructure::CompleteLocalName(name.as_str()),
-                    ])
-                    .expect("create_advertising_data error"),
-                )
-                .await;
-
-            _ = ble.cmd_set_le_advertise_enable(true).await;
-
-            log::info!("[BLE] started advertising");
-            let mut rf = |offset: usize, data: &mut [u8]| {
-                if let Ok(wifis) = signals.wifi_scan_res.try_lock() {
-                    let range = offset..wifis.len();
-                    let range_len = range.len();
-
-                    let scan_str = &wifis.as_bytes()[range];
-                    data[..range_len].copy_from_slice(scan_str);
-                    range_len
-                } else {
-                    return 0;
-                }
-            };
-
-            let mut wf = |_offset: usize, data: &[u8]| {
-                let mut tmp = [0; 128];
-                tmp[..data.len()].copy_from_slice(data);
-
-                if let Ok(mut guard) = ble_data.try_lock() {
-                    for &d in data {
-                        if d == 0x00 {
-                            ble_end_signal.signal(());
-                            break;
-                        }
-
-                        guard.push(d);
-                    }
-                }
-            };
-
-            gatt!([service {
-                uuid: "f254a578-ef88-4372-b5f5-5ecf87e65884",
-                characteristics: [characteristic {
-                    uuid: "bcd7e573-b0b2-4775-83c0-acbf3aaf210c",
-                    read: rf,
-                    write: wf,
-                }],
-            }]);
-
-            let mut rng = bleps::no_rng::NoRng;
-            let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
-            loop {
-                let fut = embassy_futures::select::select(srv.do_work(), signals.end_signalled()).await;
-                let work = match fut {
-                    First(work) => work,
-                    Second(_) => {
-                        return;
-                    }
-                };
-
-                match work {
-                    Ok(res) => {
-                        if let WorkResult::GotDisconnected = res {
-                            break;
-                        }
-                    }
-                    Err(e) => {
-                        log::error!("[BLE] work err: {e:?}");
-                    }
-                }
-
-                if ble_end_signal.signaled() {
-                    let mut guard = ble_data.lock().await;
-                    if guard.len() == 0 {
-                        continue;
-                    }
-
-                    signals.wifi_conn_info_sig.signal(guard.to_vec());
-                    guard.clear();
-
-                    let wifi_connected = signals.wifi_conn_res_sig.wait().await;
-                    if wifi_connected {
-                        return;
-                    }
-                }
-
-                Timer::after_millis(10).await;
-            }
-        }
-    */
 }
 
 async fn ble_task<C: Controller, P: PacketPool>(mut runner: Runner<'_, C, P>) {
@@ -294,6 +196,7 @@ async fn custom_task<C: Controller, P: PacketPool>(
                     .wifi_conn_info_sig
                     .signal(bytes[..bytes.len() - 1].to_vec());
 
+                _ = setup_string.set(server, &heapless::String::new());
                 let wifi_connected = signals.wifi_conn_res_sig.wait().await;
                 if wifi_connected {
                     return;
